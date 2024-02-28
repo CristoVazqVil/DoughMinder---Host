434804a522e65ff8b3fe4d193b7c45aee31b91e9
CrisVazV <cris990077@hotmail.com>
2024-02-28 11:52:27 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoughMinder___Host
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
